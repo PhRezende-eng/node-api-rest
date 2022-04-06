b1617053b7061fd214a5a85b026067d240f5dd48
Paulo <ph.hr.001@gmail.com>
2022-04-06 09:13:03 -0300

    - add a new endpoint, following the readme

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.json({ name: 'Paulo henrique' }));
+
 app.listen(3000, () => console.log('Server is running in PORT 3000 🚀'));
+
+app.get('/', (req, res) => res.json({ name: 'Paulo henrique' }));
+
+app.get('/api', (req, res) => res.send('API online'));
+
