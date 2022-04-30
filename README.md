df069179e74dd37b585f7e6f83ef5ff4269b6f31
Paulo <ph.hr.001@gmail.com>
2022-04-30 03:28:49 -0300

    CRUD to DB

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # node-api-rest
 
+## To run server (recommended: $yarn dev)
 - $npx nodemon || $npm start || $yarn dev || $yarn start
 
-<p>To run server (recommended: $yarn dev)</p>
+
+## Update|Modify a documment on dataBase
+
+```
+The top level of an update document contains one or more of the following update operators:
+
+$set - replaces the value of a field with a specified one
+$inc - increments or decrements field values
+$rename - renames fields
+$unset - removes fields
+$mul - multiplies a field value by a specified number
+```
--- src/database/connection/connection.ts
+++ src/database/connection/connection.ts
@@ -2,10 +2,13 @@ import { MongoClient } from 'mongodb';
 
 import 'dotenv/config'
 
+const dontFound = 'Object not found!\n';
+
 async function connection() {
     const uri = `${process.env.CONNECTION_TO_DATABASE}`;
 
     const client = new MongoClient(uri);
+
 
     console.log('Conectando com o dataBase...');
 
@@ -13,10 +16,14 @@ async function connection() {
 
     try {
         await client.connect();
+        // await createDocument(client, 'sample_airbnb', 'listingsAndReviews',
+        await readDocument(client, 'sample_mflix', 'movies', { title: 'Blacksmith Scene' });
+        // await updateDocument(client, 'sample_mflix', 'movies', { title: 'Blacksmith Scene' });
+        // await deleteDocument(client, 'sample_mflix', 'movies', { title: 'The Great Train Robbery' });
+
+
         // await listDatabase(client);
-        // await findOneDocument(client, 'sample_mflix', 'movies',{ title: 'Back to the Future' });
-        await readDocument(client, 'sample_mflix', 'movies', { title: 'Back to the Future' });
-        // await insertDocument(client, 'sample_airbnb', 'listingsAndReviews',
+        // await findOneDocument(client, 'sample_mflix', 'movies',{ title: 'Blacksmith Scene' });
         // {
         //     _id: 1,
         //     name: 'Paulo henrique',
@@ -42,39 +49,77 @@ async function connection() {
     }
 }
 
+async function createDocument(client: MongoClient, dataBase: String, collection: String, object: Object) {
+    const result = client.db(`${dataBase}`);
+    const getCollection = result.collection(`${collection}`);
+    const insertResponse = await getCollection.insertOne(object);
+    if (insertResponse != null) {
+        const findResponse = await getCollection.findOne({ _id: insertResponse.insertedId });
+        console.log(findResponse);
+        console.log('Created!\n');
+    } else {
+        console.log(dontFound);
+    }
+}
+
 async function readDocument(client: MongoClient, dataBase: String, collection: String, query: Object) {
     const result = client.db(`${dataBase}`);
     const getCollection = result.collection(`${collection}`);
-    const response = await getCollection.findOne(query);
-    console.log(response ? response : `Not found document`);
+    const findResponse = await getCollection.findOne(query);
+    if (findResponse != null) {
+        console.log(findResponse);
+        console.log('Readed!\n');
+    } else {
+        console.log(dontFound);
+    }
 }
 
-async function insertMultiplesDocuments(client: MongoClient, dataBase: String, collection: String, multiplesDocuments: Array<Object>) {
+async function updateDocument(client: MongoClient, dataBase: String, collection: String, query: Object) {
     const result = client.db(`${dataBase}`);
     const getCollection = result.collection(`${collection}`);
-    const response = await getCollection.insertMany(multiplesDocuments);
-    console.log(response.insertedIds);
+    const updateResponse = await getCollection.updateOne(query, { $set: { plot1: '123' } });
+    if (updateResponse != null) {
+        const findResponse = await getCollection.findOne(query);
+        console.log(findResponse);
+        console.log('Updated!\n');
+    } else {
+        console.log(dontFound);
+    }
 }
 
-async function insertDocument(client: MongoClient, dataBase: String, collection: String, object: Object) {
+async function deleteDocument(client: MongoClient, dataBase: String, collection: String, query: Object) {
     const result = client.db(`${dataBase}`);
     const getCollection = result.collection(`${collection}`);
-    const response = await getCollection.insertOne(object);
-    console.log(response.insertedId);
+    const findResponse = await getCollection.findOne(query);
+    if (findResponse != null) {
+        await getCollection.deleteOne(query);
+        console.log(findResponse);
+        console.log('Deleted!\n');
+    } else {
+        console.log(dontFound);
+    }
 }
 
-async function findOneDocument(cliente: MongoClient, dataBase: String, collection: String, query: Object) {
-    const result = cliente.db(`${dataBase}`);
-    const getCollection = result.collection(`${collection}`);
-    const response = await getCollection.findOne(query);
-    console.log(response);
-}
+// async function insertMultiplesDocuments(client: MongoClient, dataBase: String, collection: String, multiplesDocuments: Array<Object>) {
+//     const result = client.db(`${dataBase}`);
+//     const getCollection = result.collection(`${collection}`);
+//     const response = await getCollection.insertMany(multiplesDocuments);
+//     console.log(response.insertedIds);
+// }
 
-async function listDatabase(client: MongoClient) {
-    const databasesList = client.db();
-    const accessAdmin = databasesList.admin();
-    const response = await accessAdmin.listDatabases();
-    response.databases.forEach(db => console.log(`- ${db.name}`))
-}
+
+// async function findOneDocument(cliente: MongoClient, dataBase: String, collection: String, query: Object) {
+//     const result = cliente.db(`${dataBase}`);
+//     const getCollection = result.collection(`${collection}`);
+//     const response = await getCollection.findOne(query);
+//     console.log(response);
+// }
+
+// async function listDatabase(client: MongoClient) {
+//     const databasesList = client.db();
+//     const accessAdmin = databasesList.admin();
+//     const response = await accessAdmin.listDatabases();
+//     response.databases.forEach(db => console.log(`- ${db.name}`))
+// }
 
 export default connection;
